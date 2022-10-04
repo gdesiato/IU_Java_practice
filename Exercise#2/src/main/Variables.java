64bc84c49642307557ed7bc58c5195c153c8d753
Giuseppe Desiato <giodesiato@gmail.com>
2022-10-04 00:13:09 -0400

    exercise

---
+++ Exercise#2/src/main/Variables.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+//        Purpose
+//        As a programmer it is important to understand the state of the programme at any given time.
+//        When you work with variables, you should, therefore,
+//        be able to state what is inside your variable after declaration and initialization.
+//
+//        Code example
+//        Consider the following piece of code:
+        
+        // Position 1
+        short x, y;
+        // Position 2
+        x = 1234;
+        // Position 3
+        y = 99;
+        // Position 4
+        short z = x;
+        // Position 5
+        y = x;
+        // Position 6
+
+
+//        Exercise
+//        State what the values for the variables x, y and z are at each position during execution of the code.
+//        You can use the table below. If you are unsure, use the println() method to print the variable.
+
+    }
+}
