95cc8e3b362a6f349885ac6194dd898b6b81090b
Giuseppe Desiato <giodesiato@gmail.com>
2022-10-17 10:18:10 -0400

    operators

---
+++ Operators/src/main/OperatorsMain.java
@@ -0,0 +1,22 @@
+package main;
+
+public class OperatorsMain {
+
+    public static void main(String[] args) {
+
+        // integers division
+        int a = 7;
+        int b = 5;
+
+        int res = a / b;
+        System.out.println("int division. 7 / 5 = " + res);
+
+        // floating point numbers division
+        double x = 7;
+        double y = 5;
+
+        double res2 = x  / y;
+        System.out.println("double division. 7 / 5 = " + res2);
+
+    }
+}
