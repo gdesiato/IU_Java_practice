ce8d96db04576100b27b75b6f796865ca852a3b3
Giuseppe Desiato <giodesiato@gmail.com>
2022-10-17 10:34:53 -0400

    operators

--- Operators/src/main/OperatorsMain.java
+++ Operators/src/main/OperatorsMain.java
@@ -4,6 +4,10 @@ public class OperatorsMain {
 
     public static void main(String[] args) {
 
+        // Arithmetic Operators
+        // Logical Operators
+        // Relational Operators
+
         // integers division
         int a = 7;
         int b = 5;
@@ -18,5 +22,37 @@ public class OperatorsMain {
         double res2 = x  / y;
         System.out.println("double division. 7 / 5 = " + res2);
 
+
+        // relational operators
+        // == equality
+        // on primitive data types
+
+        int val1 = 10;
+        int val2 = 10;
+
+        boolean primEquality = val1 == val2;
+        System.out.println(primEquality);
+
+        // on reference data types
+
+        Obj obj1 = new Obj();
+        Obj obj2 = obj1;
+
+        boolean refEquality = obj1 == obj2;
+        System.out.println(refEquality);
+
+        // logical operators
+        // !, &&, ||, ^
+
+        // String concatenation
+
+        String s1, s2, s3, s4;
+        s1 = "Hello, ";
+        s2 = "how ";
+        s3 = "are ";
+        s4 = "you?";
+
+        String phrase = s1 + s2 + s3 + s4;
+        System.out.println(phrase);
     }
 }
