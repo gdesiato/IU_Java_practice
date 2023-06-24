dbe1f193f36e50333ca3e4ea49076947dacc4ecd
Giuseppe Desiato <giodesiato@gmail.com>
2023-06-24 07:25:07 -0400

    projectEuler_3

--- Exercise#5/src/main/DataTypes.java
+++ Exercise#5/src/main/DataTypes.java
@@ -19,62 +19,22 @@ public class DataTypes {
 //        Why does the code fail to run?
 //        How should the first line (float x = 1234.123;) be
 
-//        Find the sum of all the multiples of 3 or 5 below 1000
 
 
-//        int i;
-//        int j;
-//        int sum3 = 0;
-//        int sum5 = 0;
-//        for(i = 3; i < 1000; i = i + 3){
-//            sum3 += i;
-//        }
-//        for(j = 5; j < 1000; j = j + 5){
-//            sum5 += j;
-//        }
-//
-//        System.out.println(sum3);
-//        System.out.println(sum5);
-//        System.out.println(sum3+sum5);
-//
-//
-//        int sum1 = 0; // Variable to store the sum
-//        int sum2 = 0; // Variable to store the sum
-//        int g;
-//        int m;
-//        int count1 = 0;
-//        int count2 = 0;
-//
-//        for (g = 1; g < 1000; g++) {
-//            if (g % 3 == 0) {
-//                count1++;
-//                sum1 += g; // Add 'x' to the sum if it is a multiple of 3
-//            }
-//        }
-//
-//        for (m = 1; m < 1000; m++) {
-//            if (m % 5 == 0) {
-//                count2++;
-//                sum2 += m; // Add 'x' to the sum if it is a multiple of 5
-//            }
-//        }
-//
-//        System.out.println("The number of multiples of 3 below 1000: " + count1);
-//        System.out.println("Sum of the multiples of 3 below 1000: " + sum1);
-//
-//        System.out.println("The number of multiples of 5 below 1000: " + count2);
-//        System.out.println("Sum of the multiples of 5 below 1000: " + sum2);
-//
-//        System.out.println("Sum of the totals: " + (sum1+sum2));
 
+        Euler problem = new Euler();
 
-        calc calculator = new calc();
-
-        int a = calculator.projectEuler_1();
+        int a = problem.projectEuler_1();
         System.out.println("1. The sum of the multiples of 3 and 5 below 1000: " + a);
 
-        long b = calculator.projectEuler_2();
+        long b = problem.projectEuler_2();
         System.out.println("2. The sum of even Fibonacci numbers not exceeding 4,000,000: " + b);
+
+        boolean isPrime = problem.isPrime(14);
+        System.out.println(isPrime);
+
+        long n = 600851475143L;
+        System.out.println("The largest prime factor of " + n + " is " + problem.projectEuler_3(n));
 
     }
 
---
+++ Exercise#5/src/main/Euler.java
@@ -0,0 +1,71 @@
+package main;
+
+public class Euler {
+
+    // Find the sum of all the multiples of 3 or 5 below 1000
+    public int projectEuler_1(){
+
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i; // Add 'i' to the sum if it is a multiple of 3 or 5
+            }
+        }
+        return sum;
+    }
+
+
+    public long projectEuler_2() {
+        long t1 = 0, t2 = 1;
+
+        long sumOfEvenTerms = 0;
+
+        while (t1 <= 4000000) {
+            if(t1 % 2 == 0){
+                sumOfEvenTerms += t1;
+            }
+            long sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+        return sumOfEvenTerms;
+    }
+
+    // Is it a prime number?
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Find the largest prime factor
+    public long projectEuler_3(long n) {
+        long maxPrime = -1;
+
+        // Handle 2 as a special case
+        while (n % 2 == 0) {
+            maxPrime = 2;
+            n /= 2;
+        }
+
+        // n is odd at this point, so increment by 2
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                maxPrime = i;
+                n /= i;
+            }
+        }
+
+        // At this point, if n is a prime number greater than 2, then n itself is the largest prime
+        if (n > 2)
+            maxPrime = n;
+
+        return maxPrime;
+    }
+}
